ef22cd17224d8260c51788842448c0087f2a0c7c
Igor Faria <igor_faria@mac-02.local>
2019-04-08 11:03:30 -0300

    first commit

---
+++ ConvesoresApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ConvesoresApp
+//
+//  Created by Igor Faria on 08/04/19.
+//  Copyright © 2019 Igor Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var valueTF: UITextField!
+    @IBOutlet weak var unit1BTN: UIButton!
+    @IBOutlet weak var unit2BTN: UIButton!
+    @IBOutlet weak var resultLB: UILabel!
+    @IBOutlet weak var resultUnitLB: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    @IBAction func convert(_ sender: UIButton) {
+    }
+}
+
